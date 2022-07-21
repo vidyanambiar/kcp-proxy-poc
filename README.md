f3a9417e722e6514fe8efa692f2dde0f82489f13
Vidya Nambiar <vnambiar@redhat.com>
2022-07-21 14:55:49 -0400

    Get proxy server working
    Remove node_modules
    
    update readme

---
+++ README.md
@@ -0,0 +1,18 @@
+### Proxy requests from HAC Core to KCP (POC)
+
+This is a small POC to attempt proxying requests from a locally running HAC Core application to KCP for development purposes. The proxy is needed to add CORS headers so that the KCP API server responds to requests from HAC Core.
+
+The following environment variables need to be set:
+```bash
+# URL for KCP Server (eg. https://<kcp-host>/clusters/root:rh-sso-<org_id>).
+export KCP_SERVER_URL=...
+# Origin for HAC Core (eg. https://prod.foo.redhat.com:1337)
+export HAC_CORE_ORIGIN=...
+```
+
+After setting the environment variables, run:
+```
+node server.js
+```
+
+This will start the proxy server on port 3000.
--- server.js
+++ server.js
@@ -3,18 +3,43 @@ import cors from 'cors';
 import proxy from 'express-http-proxy';
 import url from 'url';
 
+// Check for required environment variables
+if (!process.env.KCP_SERVER_URL || !process.env.HAC_CORE_ORIGIN) {
+  throw new Error('Set environment variables KCP_SERVER_URL and HAC_CORE_ORIGIN');
+  process.exit(1);
+}
+
+const kcpServerURL = new URL(process.env.KCP_SERVER_URL);
+const kcpHost = kcpServerURL.origin;
+const kcpPath = kcpServerURL.pathname;
+const redirectPathForWorkspaces = '/services/workspaces/root:rh-sso-15850190/all';
+
 const app = express();
 
+// Add CORS headers to request
 app.use(cors({
-  origin: 'https://prod.foo.redhat.com:1337',
+  origin: process.env.HAC_CORE_ORIGIN,
   credentials: true
 }));
 
-const apiProxy = proxy('https://kcp-stable.apps.kcp-internal-1.52p3.p1.openshiftapps.com/clusters/root:rh-sso-15850190', {
-  proxyReqPathResolver: req => url.parse(req.originalUrl).path
+// Proxy to KCP server
+const apiProxy = proxy(kcpHost, {
+  proxyReqPathResolver: req => {
+    const requestedPath = url.parse(req.originalUrl).path;
+    let updatedPath = kcpPath + url.parse(req.originalUrl).path;
+    /**
+     * The endpoint /clusters/root:rh-sso-1585019/apis/tenancy.kcp.dev/v1beta1/workspaces
+     * redirects to /services/workspaces/system:admin/all/apis/tenancy.kcp.dev/v1beta1/workspaces
+     * and we need to add the CORS header on the redirect as well
+     */
+    if (requestedPath.includes('/apis/tenancy.kcp.dev/v1beta1/workspaces')) {
+      updatedPath = redirectPathForWorkspaces + requestedPath;
+    }
+    return updatedPath;
+  }
 });
 
-app.use('/apis*', apiProxy);
+app.use('/apis', apiProxy);
 
 app.listen(3000, () => {
   console.log('Proxy server listening on port 3000');
