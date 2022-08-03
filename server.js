9d61d3d35b2cf49ee0c207d402a4dd093d863610
Vidya Nambiar <vnambiar@redhat.com>
2022-08-03 08:51:54 -0400

    Proxy requests for api/v1 path

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ const apiProxy = proxy(kcpHost, {
   proxyReqPathResolver: req => {
     const requestedPath = url.parse(req.originalUrl).path;
     let updatedPath = kcpPath + url.parse(req.originalUrl).path;
+    if (requestedPath === '/api/v1') {
+      console.log('updatedPath: ', updatedPath);
+    }
     /**
      * The endpoint /clusters/root:rh-sso-1585019/apis/tenancy.kcp.dev/v1beta1/workspaces
      * redirects to /services/workspaces/system:admin/all/apis/tenancy.kcp.dev/v1beta1/workspaces
@@ -36,10 +39,10 @@ const apiProxy = proxy(kcpHost, {
       updatedPath = redirectPathForWorkspaces + requestedPath;
     }
     return updatedPath;
-  }
+  },
 });
 
-app.use('/apis', apiProxy);
+app.use(['/apis', '/api/v1'], apiProxy);
 
 app.listen(3000, () => {
   console.log('Proxy server listening on port 3000');
